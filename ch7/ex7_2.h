fa5b3dfce25e5e2d54f699a34f558489ff1434a5
siyuancheng <syuan.cheng21@gmail.com>
2023-06-24 16:07:24 +0800

    ex7_2

---
+++ ch7/ex7_2.h
@@ -0,0 +1,23 @@
+//
+//ex7_2.h
+// Exercise 7.2, P233
+// 
+
+#include <iostream>
+
+struct Sales_data
+{
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+
+    std::string isdn() const { return bookNo; }
+    Sales_data& combine(const Sales_data& rhs);
+};
+
+Sales_data& Sales_data::combine(const Sales_data& rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+
+    return *this;
+}
