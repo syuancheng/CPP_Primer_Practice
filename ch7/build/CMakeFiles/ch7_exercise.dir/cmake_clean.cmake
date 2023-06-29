e97ef898dd6ef4a7bae307ee0cfef3e7b1cc2d7b
siyuancheng <syuan.cheng21@gmail.com>
2023-06-29 22:10:30 +0800

    7.5.3

---
+++ ch7/build/CMakeFiles/ch7_exercise.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ch7_exercise.dir/ex7_41.cpp.o"
+  "CMakeFiles/ch7_exercise.dir/ex7_41.cpp.o.d"
+  "ch7_exercise"
+  "ch7_exercise.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ch7_exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch7/ex7_41.cpp
@@ -0,0 +1,13 @@
+#include "ex7_41.h"
+
+using std::cout;
+using std::endl;
+
+int main() {
+    cout << "1. default way: " << endl;
+    cout << "----------------" << endl;
+    Sales_data s1;
+
+
+    return 0;
+}
