46e61c1d554cd199d3be045573218c69646476e9
siyuancheng <syuan.cheng21@gmail.com>
2023-07-02 17:53:48 +0800

    fix compile error

--- ch13/ex13_19_TEST.cpp
+++ ch13/ex13_19_TEST.cpp
@@ -5,11 +5,11 @@
 int main() {
     Employee e1("krystal");
 
-    Employee e2 = e1;//unsupport
+    // Employee e2 = e1;//unsupport
 
     Employee e3;
 
-    e3 = e1; //unsupport
+    // e3 = e1; //unsupport
 
 
     return 0;
