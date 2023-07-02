07486e26824681cd9f9e5d4b5127268abd5b942e
siyuancheng <syuan.cheng21@gmail.com>
2023-07-02 14:45:21 +0800

    13.1

---
+++ ch13/ex13_19.h
@@ -0,0 +1,28 @@
+#ifndef SYUAN_EX13_18_H
+#define SYUAN_EX13_18_H
+
+#include <string>
+
+class Employee {
+private:
+    std::string name;
+    int id;
+
+    static int unique_id;
+
+public:
+    Employee() {
+        id = unique_id++;
+    }
+    Employee(const std::string& n) {
+        id = unique_id++;
+        name = n;
+    }
+    Employee(const Employee&)=delete;
+    Employee& operator=(const Employee&)=delete;
+};
+
+int Employee::unique_id = 1;
+
+
+#endif // SYUAN_EX13_18_H
