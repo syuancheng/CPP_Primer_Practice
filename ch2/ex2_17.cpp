f7ffdfe14092c43fe0342716fc7cc0af0594953e
siyuancheng <syuan.cheng21@gmail.com>
2023-07-08 00:56:22 +0800

    ex 2.17

---
+++ ch2/ex2_17.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+int main() {
+  int i, &ri = i;
+  i = 5;
+  ri = 10;
+  cout << i << " " << ri << endl;
+
+  int ni = 20;
+
+  ri = ni; 
+    cout << i << " " << ri << endl;
+    return 0;
+}
