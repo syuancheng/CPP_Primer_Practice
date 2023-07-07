6ee84f17c234ff41f370c918aff09f46dd8e97d9
siyuancheng <syuan.cheng21@gmail.com>
2023-07-07 23:46:25 +0800

    update

--- ch13/ex13_13.h
+++ ch13/ex13_13.h
@@ -6,21 +6,17 @@
 using std::cout;
 using std::endl;
 
-struct X
-{
-    X() { cout << "X()" << endl; }
-    X(const X&) { cout << "X(const X&)" << endl; }
+struct X {
+  X() { cout << "X()" << endl; }
+  X(const X &) { cout << "X(const X&)" << endl; }
 
-    X& operator=(const X&){
-        cout << "operator=" << endl;
+  X &operator=(const X &) {
+    cout << "operator=" << endl;
 
-        return *this;
-    }
+    return *this;
+  }
 
-    ~X() {
-        cout << "~X()" << endl;
-    }
+  ~X() { cout << "~X()" << endl; }
 };
-
 
 #endif // SYUAN_EX13_13_H
