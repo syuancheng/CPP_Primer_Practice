876b9815744e2de95589730cfef6d4e28ab429ca
Siyuan Cheng <siyuan.cheng@shopee.com>
2024-03-30 12:05:32 +0800

    update

--- ch12/ex12_10.cpp
+++ ch12/ex12_10.cpp
@@ -9,10 +9,10 @@ int main() {
   std::shared_ptr<int> p(new int(42));
   std::cout << "outside the process function: " << p.use_count() << "\n";
 
-//   auto p1 = std::shared_ptr<int>(p);
-//   process(p1);//3
+  //   auto p1 = std::shared_ptr<int>(p);
+  //   process(p1);//3
 
-    process(std::shared_ptr<int>(p)); //2
+  process(std::shared_ptr<int>(p)); // 2
   std::cout << "outside the process function: " << p.use_count() << "\n";
 
   return 0;
