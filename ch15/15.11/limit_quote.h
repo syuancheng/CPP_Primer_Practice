96ea8ad8162976c41ab3d8414f48c2c0e66c9151
Siyuan Cheng <siyuan.cheng@shopee.com>
2024-06-08 19:56:04 +0800

    move func impl in head file

--- ch15/15.11/limit_quote.h
+++ ch15/15.11/limit_quote.h
@@ -8,20 +8,24 @@ public:
   Limit_quote(std::string b, double p, size_t m, double d)
       : Quote(b, p), min_count(m), discount(d) {}
 
-  double net_price(size_t s) const override {
-    if (s > min_count) {
-      return s * price * (1 - discount);
-    }
-    return s * price;
-  }
+  double net_price(size_t s) const override;
 
-  void debug() const override {
-    Quote::debug();
-    std::cout << "min_count= " << this->min_count
-              << " ,discount= " << this->discount;
-  }
+  void debug() const override;
 
 private:
   size_t min_count;
   double discount = 0.0;
 };
+
+double Limit_quote::net_price(size_t s) const {
+  if (s > min_count) {
+    return s * price * (1 - discount);
+  }
+  return s * price;
+}
+
+void Limit_quote::debug() const {
+  Quote::debug();
+  std::cout << "min_count= " << this->min_count
+            << " ,discount= " << this->discount;
+}
--- ch15/15.11/quote.h
+++ ch15/15.11/quote.h
@@ -11,12 +11,8 @@ public:
 
   std::string isbn() const { return bookNo; }
 
-  virtual double net_price(size_t s) const { return s * price; }
-  virtual void debug() const {
-    std::cout << "data members of this class:\n"
-              << "bookNo= " << this->bookNo << " "
-              << "price= " << this->price << " ";
-  }
+  virtual double net_price(size_t s) const;
+  virtual void debug() const;
 
   virtual ~Quote() = default;
 
@@ -26,5 +22,13 @@ private:
 protected:
   double price = 0.0;
 };
+
+double Quote::net_price(size_t s) const { return s * price; }
+
+void Quote::debug() const {
+  std::cout << "data members of this class:\n"
+            << "bookNo= " << this->bookNo << " "
+            << "price= " << this->price << " ";
+}
 
 #endif /* E3DF5D34_0F68_4D59_B6F2_4B9B6375FCB8 */
